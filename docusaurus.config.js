f28dbf5af806648a43e76fe8208572de5647c466
RidgeFoo <16745217+RidgeFoo@users.noreply.github.com>
2024-10-15 21:34:00 +0100

    GH pages deployment config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,15 +13,17 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://melangemetrics.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/melange-metrics/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'RidgeFoo', // Usually your GitHub org/user name.
+  projectName: 'melange-metrics', // Usually your repo name.
+  deploymentBranch: 'gh-pages',
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
