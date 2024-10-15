3755864635686c7feac6928d7b097bf07ad7d370
RidgeFoo <16745217+RidgeFoo@users.noreply.github.com>
2024-10-16 00:06:07 +0100

    Change base url for custom domain

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
   url: 'https://melangemetrics.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/melange-metrics/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
